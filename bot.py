6e78d6ba0bd90c17b0770f25f6973feb93ac27d8
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-30 15:58:03 +0200

    basic bot

---
+++ bot.py
@@ -0,0 +1,33 @@
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+
+from gpt import *
+from util import *
+
+async def start(update,context):
+    text = load_message("main")
+    await send_photo(update, context, "main")
+    await send_text(update, context, text)
+
+async def hello(update, context):
+    await send_text(update, context,"*Привет*")
+    await send_text(update, context, "_Как дела?_")
+    await send_text(update, context, "Вы написали "+update.message.text)
+    await send_photo(update, context, "avatar_main")
+    await send_text_buttons(update,context,"Запустить процесс?",{
+        "start": "Запустить",
+        "stop": "Остановить"
+    })
+
+
+async def hello_button(update, context):
+    query = update.callback_query.data
+    if query== "start":
+        await send_text(update,context,"Процесс запущен")
+    else:
+        await send_text(update,context,"Процесс остановлен")
+
+app = ApplicationBuilder().token("8175570126:AAGAKSJai7PzR-rnmxmjw4jJF2i8t4TRZgo").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , hello))
+app.add_handler(CallbackQueryHandler(hello_button))
+app.run_polling()
