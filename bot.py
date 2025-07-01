704b414d7bc9ea93f9be5c3b206df33cd7dba7cf
Maksym Rubtsov <maksrub04@gmail.com>
2025-07-01 11:46:03 +0200

    commands

--- bot.py
+++ bot.py
@@ -3,31 +3,68 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQu
 from gpt import *
 from util import *
 
-async def start(update,context):
+
+async def start(update, context):
+    dialog.mode = "main"
     text = load_message("main")
     await send_photo(update, context, "main")
     await send_text(update, context, text)
 
-async def hello(update, context):
-    await send_text(update, context,"*Привет*")
-    await send_text(update, context, "_Как дела?_")
-    await send_text(update, context, "Вы написали "+update.message.text)
-    await send_photo(update, context, "avatar_main")
-    await send_text_buttons(update,context,"Запустить процесс?",{
-        "start": "Запустить",
-        "stop": "Остановить"
+    await show_main_menu(update,context,{
+        "start": "главное меню бота",
+        "profile": "генерация Tinder-профля 😎",
+        "opener": "сообщение для знакомства 🥰",
+        "message": "переписка от вашего имени 😈",
+        "date": "переписка со звездами 🔥",
+        "gpt": "задать вопрос чату GPT 🧠 "
     })
+
+
+async def gpt(update, context):
+    dialog.mode = "gpt"
+    text = load_message("gpt")
+    await send_text(update, context, "gpt")
+    await send_text(update, context, text)
+
+
+async def gpt_dialog(update, context):
+    text = update.message.text
+    promt = load_prompt("gpt")
+    answer = await chatgpt.send_question(promt, text)
+    await send_text(update, context, answer)
+
+
+async def hello(update, context):
+    if dialog.mode == "gpt":
+        await gpt_dialog(update, context)
+    else:
+        await send_text(update, context, "*Привет*")
+        await send_text(update, context, "_Как дела?_")
+        await send_text(update, context, "Вы написали " + update.message.text)
+        await send_photo(update, context, "avatar_main")
+        await send_text_buttons(update, context, "Запустить процесс?", {
+            "start": "Запустить",
+            "stop": "Остановить"
+        })
 
 
 async def hello_button(update, context):
     query = update.callback_query.data
-    if query== "start":
-        await send_text(update,context,"Процесс запущен")
+    if query == "start":
+        await send_text(update, context, "Процесс запущен")
     else:
-        await send_text(update,context,"Процесс остановлен")
+        await send_text(update, context, "Процесс остановлен")
+
+
+dialog = Dialog()
+dialog.mode = None
+
+chatgpt = ChatGptService(
+    token="javcgk8pFZZssAv/GAaLFtpU2XRxcYwvevXZIyGFAmFZI3L06qepS/RV1vwI5WWJCzXUXxhEgBL78gsqw097AFLgJhfSJIYTGLJXuNmmC1WHnD5rLqK5bovPAMquTedVct0tMO3YKL7WnwWVBYot49YP/DsQPPKt8po+UHHV7OmqYXjjYWW2CcTundXGhGuyvJm5sNKlWWp5DZqBhLIWahLjBMGuOH0m3XMutHaIG8dtbZZqI=")
 
 app = ApplicationBuilder().token("your-tokern").build()
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , hello))
+app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
